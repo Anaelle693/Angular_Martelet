704ff6c57fb56f5fc4a32e1d1bb08b481f587a26
Anaelle693 <anaellev@icloud.com>
2021-06-23 16:39:19 +0200

    first commit

---
+++ src/app/core/services/version.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VersionService {
+
+  public version$ = new BehaviorSubject(1);
+
+  constructor() { }
+}
