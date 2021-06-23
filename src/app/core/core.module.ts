8ea944b7afb054dde738b0fc00a14ae3c9917291
Anaelle693 <anaellev@icloud.com>
2021-06-23 12:11:04 +0200

    first commit

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavComponent } from './components/nav/nav.component';
+import { IconsModule } from '../icons/icons.module';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -11,14 +13,17 @@ import { NavComponent } from './components/nav/nav.component';
     FooterComponent,
     HeaderComponent,
     NavComponent
+
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     FooterComponent,
     HeaderComponent,
-    NavComponent
+    NavComponent,
+    IconsModule
   ]
 })
 export class CoreModule { }
