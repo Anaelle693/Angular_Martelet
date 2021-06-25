e9c6f4b9cf734fc4146891927c8ecdfdb091195d
Anaelle693 <anaellev@icloud.com>
2021-06-25 15:10:29 +0200

    first commit

--- src/app/login/pages/page-sign-in/page-sign-in.component.ts
+++ src/app/login/pages/page-sign-in/page-sign-in.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-sign-in.component.scss']
 })
 export class PageSignInComponent implements OnInit {
+  public titre = 'Accueil';
+  public nb: number = 0
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  changeTitre(){
+    this.nb++;
+    this.titre=`J'ai changé de titre ${this.nb} fois `;
   }
 
 }
